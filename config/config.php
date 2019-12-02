b9489d2d7292c7ee6de1bed07f7780836429414e
Chin Leung <hello@chinleung.com>
2019-12-02 13:50:36 -0500

    Added default locale to config

--- config/config.php
+++ config/config.php
@@ -2,10 +2,26 @@
 
 return [
     /*
-     * The list of locales that are supported by the application.
-     * This can be overwritten by setting a 'app.locales'.
-     */
+    |--------------------------------------------------------------------------
+    | Application Locales Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The list of locales that are supported by the application.
+    |
+    */
+
     'supported' => [
         'en',
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The default locale of the application.
+    |
+    */
+
+    'default' => config('app.fallback_locale', 'en'),
 ];
