192d12554d86259a04f1a2a0c0f1d445c1856b69
Chin Leung <hello@chinleung.com>
2025-04-18 12:29:04 -0400

    Fixed the macro

--- src/Macros/AppMacros.php
+++ src/Macros/AppMacros.php
@@ -2,22 +2,26 @@
 
 namespace ChinLeung\LaravelLocales\Macros;
 
+use Closure;
+
 class AppMacros
 {
     /**
      * Retrieve the supported locales of the application.
      */
-    public function getLocales(?array $locales = null): array
+    public function getLocales(?array $locales = null): Closure
     {
-        if (! is_null($locales)) {
-            config([
-                'app.locales' => $locales,
-                'locales.supported' => $locales,
-            ]);
-        }
+        return function () use ($locales) {
+            if (! is_null($locales)) {
+                config([
+                    'app.locales' => $locales,
+                    'locales.supported' => $locales,
+                ]);
+            }
 
-        $locales = config('app.locales') ?? config('locales.supported');
+            $locales = config('app.locales') ?? config('locales.supported');
 
-        return isset($locales[0]) ? $locales : array_keys($locales);
+            return isset($locales[0]) ? $locales : array_keys($locales);
+        };
     }
 }
